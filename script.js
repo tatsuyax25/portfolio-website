a2cc3deae7c52b6d557d7505fa40d64e24d01fdc
Miguel Urena <urena_miguel82@outlook.com>
2024-11-22 13:11:32 -0800

    Update Smooth Scroll

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Example: Smooth Scroll to section
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+  anchor.addEventListener('click', function (e) {
+    const targetId = this.getAttribute('href');
+    if (targetId && targetId.startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
   });
 });
 
